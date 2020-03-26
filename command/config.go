3f2b14f5beb0499dfcb8504396299f59a10d8d49
vilmibm <vilmibm@neongrid.space>
2020-03-26 17:19:45 -0500

    stubs

--- command/config.go
+++ command/config.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 )
 
@@ -20,5 +21,35 @@ var cfgCmd = &cobra.Command{
 func cfg(cmd *cobra.Command, args []string) error {
 	// TODO decide on top level behavior. I'm thinking either just dump the config or go into an
 	// interactive mode where config keys can be searched through.
+
+	// otherwise, we'll dispatch on arg length for now.
+
+	switch len(args) {
+	case 0:
+		cfgInteractive(cmd)
+	case 1:
+		cfgGet(cmd, args[0])
+	case 2:
+		cfgSet(cmd, args[0], args[1])
+	default:
+		fmt.Println("i have no idea what you are saying to me")
+	}
+
+	return nil
+}
+
+func cfgInteractive(cmd *cobra.Command) error {
+	fmt.Println("cool interactive thing")
+	return nil
+}
+
+func cfgGet(cmd *cobra.Command, key string) error {
+	fmt.Println("getting", key)
+	return nil
+}
+
+func cfgSet(cmd *cobra.Command, key, value string) error {
+	fmt.Println("setting", key, "to", value)
+
 	return nil
 }
